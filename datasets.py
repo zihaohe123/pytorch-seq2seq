bd4643eb26bea8601e98c2c51ef2f387fe9d06eb
Johnny Wei <johntzwei@yahoo.com>
2020-02-09 21:26:37 -0800

    revert dataset in previous commit that was causing bert bugs

--- datasets.py
+++ datasets.py
@@ -1,17 +1,17 @@
 from torchtext.data import Field, BucketIterator
 from torchtext.datasets import Multi30k, IWSLT, TranslationDataset
+
 from transformers import BertTokenizer
 
-
-def multi30k(device, batch_size, bert, train_path, dev_path, test_path):
+def multi30k(device, batch_size, lower, tokenizer, train_path, dev_path, test_path):
     print('Loading data with torchtext...')
     source = Field(
             sequential=True,
             use_vocab=True,
             init_token='[CLS]',
             eos_token='[SEP]',
-            lower=not bert,
-            tokenize=BertTokenizer.from_pretrained('bert-base-german-cased').encode if bert else str.split,
+            lower=lower,
+            tokenize=tokenizer,
             include_lengths=True,
             batch_first=False,
             pad_token='<pad>',
@@ -48,24 +48,37 @@ def multi30k(device, batch_size, bert, train_path, dev_path, test_path):
     return (source, target), (train_iterator, val_iterator, test_iterator)
 
 
-def iwslt2014(device, batch_size, bert, train_path, dev_path, test_path):
+def iwslt2014(device, batch_size, bert, train_path, dev_path, test_path, bert=True):
     return bpe_dataset(device, bert, train_path, dev_path, test_path, batch_size)
 
 
 def bpe_dataset(device, bert, train_path, dev_path, test_path, batch_size):
     print('Loading data with torchtext...')
-    source = Field(
-            sequential=True,
-            use_vocab=True,
-            init_token='[CLS]',
-            eos_token='[SEP]',
-            tokenize=BertTokenizer.from_pretrained('bert-base-german-cased').encode if bert else str.split,
-            include_lengths=True,
-            batch_first=False,
-            pad_token='<pad>',
-            unk_token='<unk>'
-        )
-
+    if bert:
+        tokenizer = BertTokenizer.from_pretrained('bert-base-german-cased').encode
+        source = Field(
+                sequential=True,
+                use_vocab=False,
+                init_token='[CLS]',
+                eos_token='[EOS]',
+                tokenize=tokenizer,
+                include_lengths=True,
+                batch_first=False,
+                pad_token='<pad>',
+                unk_token='<unk>'
+            )
+    else:
+        source = Field(
+                sequential=True,
+                use_vocab=True,
+                init_token='<SOS>',
+                eos_token='<EOS>',
+                tokenize=str.split,
+                include_lengths=True,
+                batch_first=False,
+                pad_token='<pad>',
+                unk_token='<unk>'
+            )
 
     target = Field(
             sequential=True,
